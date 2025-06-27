6bcec11384ab6b8d9891143964f520d481a75217
Cleyton Gravito <cleytong122@gmail.com>
2025-06-27 12:02:55 -0300

    Atualização.

--- Gestao-Escolar/Gestao-Escolar/Properties/Resources.Designer.cs
+++ Gestao-Escolar/Gestao-Escolar/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Gestao_Escolar.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Logo {
+            get {
+                object obj = ResourceManager.GetObject("Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Senha {
             get {
                 object obj = ResourceManager.GetObject("Senha", resourceCulture);
